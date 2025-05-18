c2ddd364e0d8301c7b521dae02b0ef7dac844dbf
garesoft <subcity@me.com>
2025-05-18 10:48:55 -0400

    init commit dafuq

---
+++ nvim/.config/nvim/lua/plugins/spec1.lua
@@ -0,0 +1,45 @@
+
+
+return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	
+	{ 
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate", 
+		highlight = {
+			enable = true
+		}
+	}, 
+	{
+		'nvimdev/dashboard-nvim',
+		 event = 'VimEnter',
+		 config = function()
+			 local ascii = require("ascii.catgirl")
+			require('dashboard').setup {
+		      -- config
+		      		theme = 'doom',
+				config = {
+					header = ascii.cat
+				}
+		    	}
+		  end,
+		  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	},
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
+	{
+	  "nvim-neo-tree/neo-tree.nvim",
+	  branch = "v3.x",
+	  dependencies = {
+	    "nvim-lua/plenary.nvim",
+	    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	    "MunifTanjim/nui.nvim",
+	    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+	  },
+	  lazy = false, -- neo-tree will lazily load itself
+	  ---@module "neo-tree"
+	  ---@type neotree.Config?
+	  opts = {
+	    -- fill any relevant options here
+	  },
+	}
+}
